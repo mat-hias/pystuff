71606bb2d59b2fea92cb6b04224f1435a2af842d
mat-hias <mat-hias@users.noreply.github.com>
2023-08-02 16:49:41 +0200

    1st commit for gittest.py

---
+++ gittest.py
@@ -0,0 +1,8 @@
+fruits = ["apple", "banana", "cherry"]
+newFruits = []
+
+for f in fruits:
+    newFruits.append(f)
+
+print(newFruits)
+print('====================================================================')
